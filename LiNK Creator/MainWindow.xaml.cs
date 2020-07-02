f179ed89b9eeefbaa6846acf43c6869c8d5c63cc
Christian Borck <borck@hotmail.de>
2020-07-03 00:13:11 +0200

    [UPDATE] LiNK Creator to use MahApps.Metro (dark steel theme)

--- LiNK Creator/MainWindow.xaml.cs
+++ LiNK Creator/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace LiNK_Creator {
   /// <summary>
   ///   Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class MainWindow : Window {
+  public partial class MainWindow {
     private static Settings Settings => Settings.Default;
 
     private string ScanDirectory => ScanCombo.Text;
