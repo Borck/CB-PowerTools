1f2291df1bdfe3492949640e6bb05976e279a8de
Christian Borck <borck@hotmail.de>
2020-08-03 22:29:57 +0200

    [UPDATE] unhandled exception will now be logged instead of crashing the application (may not be the best solution)

--- LiNK Creator/App.xaml.cs
+++ LiNK Creator/App.xaml.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using NLog;
+
+
 
 namespace LiNK_Creator {
   /// <summary>
-  /// Interaction logic for App.xaml
+  ///   Interaction logic for App.xaml
   /// </summary>
   public partial class App : Application {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+
+    protected override void OnStartup(StartupEventArgs e) {
+      base.OnStartup(e);
+
+      SetupExceptionHandling();
+    }
+
+
+
+    private void SetupExceptionHandling() {
+      AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                                                      LogUnhandledException(
+                                                        (Exception)e.ExceptionObject,
+                                                        "AppDomain.CurrentDomain.UnhandledException"
+                                                      );
+
+      DispatcherUnhandledException += (s, e) => {
+                                        LogUnhandledException(
+                                          e.Exception,
+                                          "Application.Current.DispatcherUnhandledException"
+                                        );
+                                        e.Handled = true;
+                                      };
+
+      TaskScheduler.UnobservedTaskException += (s, e) => {
+                                                 LogUnhandledException(
+                                                   e.Exception,
+                                                   "TaskScheduler.UnobservedTaskException"
+                                                 );
+                                                 e.SetObserved();
+                                               };
+    }
+
+
+
+    private void LogUnhandledException(Exception exception, string source) {
+      var message = $"Unhandled exception ({source})";
+      try {
+        var assemblyName = Assembly.GetExecutingAssembly().GetName();
+        message = $"Unhandled exception in {assemblyName.Name} v{assemblyName.Version}";
+      } catch (Exception ex) {
+        Logger.Error(ex, "Exception in LogUnhandledException");
+      } finally {
+        Logger.Error(exception, message);
+      }
+    }
   }
 }
